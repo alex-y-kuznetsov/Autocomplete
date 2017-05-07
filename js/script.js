ac62976854c0a92a3b8c9d505376f0afa206befe
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-05-07 22:02:05 +0500

    created update form function

--- js/script.js
+++ js/script.js
@@ -17,9 +17,32 @@ var toggleResultCity = function () {
   resultCity.style.display = "none";
 }
 
+//Get results
 var updateResults = function () {
   if (!autocompleteCity.value) {
     toggleResultCity();
     return;
   }
+  var a = new RegExp("^" + autocompleteCity.value, "i");
+  for(var x = 0, b = document.createDocumentFragment(), c = false; x < cities.length; x++) {
+    if(a.test(cities[x])) {
+      c = true;
+      var d = document.createElement("p");
+      d.innerText = cities[x];
+      d.setAttribute("onclick", "autocompleteCity.value=this.innerText;resultCity.innerHTML=';resultCity.style.display='none';");
+      b.appendChild(d);
+    }
+  }
+  if(c === true) {
+    resultCity.innerHTML = '';
+    resultCity.style.display = "block";
+    resultCity.appendChild(b);
+    return;
+  }
+  toggleResultCity();
 }
+
+//Listeners
+autocompleteCity.addEventListener("keydown", updateResults);
+autocompleteCity.addEventListener("change", updateResults);
+autocompleteCity.addEventListener("focus", updateResults);
