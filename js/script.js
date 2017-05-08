3aa7ba8dcfc7503a0bd1594c096f63473eb22ffc
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-05-08 18:46:40 +0500

    linked list of cities from another file

--- js/script.js
+++ js/script.js
@@ -3,13 +3,7 @@
 //Variables
 var resultCity = document.getElementById("result-city");
 var autocompleteCity = document.getElementById("autocomplete-city");
-var cities = [
-  "Екатеринбург",
-  "Ирбит",
-  "Рейкьявик",
-  "Лондон",
-  "Бельдяжки",
-];
+var cities = window.citiesArray;
 
 //Reset results
 var toggleResultCity = function () {
@@ -25,10 +19,10 @@ var updateResults = function () {
   }
   var a = new RegExp("^" + autocompleteCity.value, "i");
   for(var x = 0, b = document.createDocumentFragment(), c = false; x < cities.length; x++) {
-    if(a.test(cities[x])) {
+    if(a.test(cities[x].City)) {
       c = true;
       var d = document.createElement("p");
-      d.innerText = cities[x];
+      d.innerText = cities[x].City;
       d.setAttribute("onclick", "autocompleteCity.value=this.innerText;resultCity.innerHTML=';resultCity.style.display='none';");
       b.appendChild(d);
     }
