13e1148cfc05eb88cb2ed8b45101902a93289fab
alex-y-kuznetsov <alex.y.kuznetsov@gmail.com>
2017-05-09 22:02:43 +0500

    script fixes

--- js/script.js
+++ js/script.js
@@ -18,20 +18,20 @@ var updateResults = function () {
     toggleResultCity();
     return;
   }
-  var a = new RegExp("^" + autocompleteCity.value, "i");
-  for(var x = 0, b = document.createDocumentFragment(), c = false; x < cities.length; x++) {
-    if(a.test(cities[x].City)) {
-      c = true;
-      var d = document.createElement("p");
-      d.innerText = cities[x].City;
-      d.setAttribute("onclick", "autocompleteCity.value=this.innerText;resultCity.innerHTML='';resultCity.style.display='none';");
-      b.appendChild(d);
+  var citySearcher = new RegExp("^" + autocompleteCity.value, "i");
+  for(var x = 0, cityItem = document.createDocumentFragment(), cityChecker = false; x < cities.length; x++) {
+    if(citySearcher.test(cities[x].City)) {
+      cityChecker = true;
+      var cityName = document.createElement("p");
+      cityName.innerText = cities[x].City;
+      cityName.setAttribute("onclick", "autocompleteCity.value=this.innerText;resultCity.innerHTML='';resultCity.style.display='none';");
+      cityItem.appendChild(cityName);
     }
   }
-  if(c === true) {
+  if(cityChecker === true) {
     resultCity.innerHTML = "";
     resultCity.style.display = "block";
-    resultCity.appendChild(b);
+    resultCity.appendChild(cityItem);
     return;
   } else {
     resultCity.innerText = "Не найдено";
@@ -45,6 +45,6 @@ var updateResults = function () {
 autocompleteCity.addEventListener("keyup", updateResults);
 autocompleteCity.addEventListener("change", updateResults);
 autocompleteCity.addEventListener("focus", updateResults);
-autocompleteCity.addEventListener("keyup", function(e){
+autocompleteCity.addEventListener("keyup", function(e) {
   e.keyCode == ESCAPE_KEY_CODE && (resultCity.style.display = "none")
-}, false);
+});
